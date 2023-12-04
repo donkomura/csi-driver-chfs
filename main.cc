9a4973cae6f777ecf356d902b85133bdb7324083
donkomura <koiru3822fs@gmail.com>
2023-12-04 23:20:12 +0900

    format

--- main.cc
+++ main.cc
@@ -1,36 +1,32 @@
-#include <iostream>
-#include <fstream>
-
 #include <grpc/support/log.h>
 #include <grpcpp/grpcpp.h>
+
 #include <cxxopts.hpp>
+#include <fstream>
+#include <iostream>
 
 const std::string VERSION_FILE = "VERSION";
 
-int main(int argc, char **argv)
-{
-    cxxopts::Options options("chfsplugin", "CSI driver for CHFS");
-    options.add_options()("h,help", "Print this message")("v,version", "version");
+int main(int argc, char **argv) {
+  cxxopts::Options options("chfsplugin", "CSI driver for CHFS");
+  options.add_options()("h,help", "Print this message")("v,version", "version");
 
-    auto parsed = options.parse(argc, argv);
-    if (parsed.count("help"))
-    {
-        std::cout << options.help() << std::endl;
-        exit(0);
+  auto parsed = options.parse(argc, argv);
+  if (parsed.count("help")) {
+    std::cout << options.help() << std::endl;
+    exit(0);
+  }
+  if (parsed.count("version")) {
+    std::ifstream ifs(VERSION_FILE);
+    std::string version;
+    if (ifs.fail()) {
+      std::cout << "version file \'" << VERSION_FILE << "\' does not found"
+                << std::endl;
+      exit(1);
     }
-    if (parsed.count("version"))
-    {
-        std::ifstream ifs(VERSION_FILE);
-        std::string version;
-        if (ifs.fail())
-        {
-            std::cout << "version file \'" << VERSION_FILE << "\' does not found" << std::endl;
-            exit(1);
-        }
-        while (getline(ifs, version))
-        {
-            std::cout << version << std::endl;
-            exit(0);
-        }
+    while (getline(ifs, version)) {
+      std::cout << version << std::endl;
+      exit(0);
     }
+  }
 }
