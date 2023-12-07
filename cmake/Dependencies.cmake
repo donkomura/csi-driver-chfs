04e6a0a6b88dd39478f53c9fa781bafbab6d5653
donkomura <koiru3822fs@gmail.com>
2023-12-07 20:36:26 +0900

    move and install proto definition

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -27,9 +27,9 @@ macro(install_dependencies)
     FetchContent_MakeAvailable(cxxopts)
 
     # install CSI spec
-    set(external_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/external")
+    set(external_PREFIX "${CMAKE_CURRENT_BINARY_DIR}")
     set(external_SOURCE_DIR "${external_PREFIX}/src")
-    set(external_INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/install")
+    set(external_INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/csi-spec")
     ExternalProject_Add(
         csi-spec
         GIT_REPOSITORY https://github.com/donkomura/csi-spec.git
@@ -41,14 +41,21 @@ macro(install_dependencies)
         BUILD_IN_SOURCE 1
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ${MAKE_EXE} build_cpp PROTOC_VER=24.4
-        INSTALL_COMMAND sh -c "cp ${external_SOURCE_DIR}/csi-spec/lib/cxx/csi/* ${external_INSTALL_DIR}"
+        INSTALL_COMMAND sh -c "\
+            cp ${external_SOURCE_DIR}/csi-spec/lib/cxx/csi/* ${external_INSTALL_DIR} && \
+            cp ${external_SOURCE_DIR}/csi-spec/csi.proto ${external_INSTALL_DIR}"
     )
 
     if(external_INSTALL_DIR)
+        set(csi-spec_SOURCE_DIR "${external_SOURCE_DIR}/csi-spec")
         set(csi-spec_INSTALL_DIR "${external_INSTALL_DIR}")
         set(csi-spec_INCLUDE_DIR "${csi-spec_INSTALL_DIR}")
-        set(csi-spec_SOURCE "${csi-spec_INSTALL_DIR}/csi.pb.cc")
+        set(csi-spec_PROTOC_INCLUDE_DIR "${csi-spec_SOURCE_DIR}/lib/cxx/.protoc/include")
+        set(csi-spec_PROTO_GEN_SRC "${csi-spec_INSTALL_DIR}/csi.pb.cc")
+        set(csi-spec_PROTO_GEN_HDR "${csi-spec_INSTALL_DIR}/csi.pb.h")
     endif()
 
     add_dependencies(grpc csi-spec)
+
+    include(cmake/GrpcGen.cmake)
 endmacro()
