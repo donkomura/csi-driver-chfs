d0f8f543e6e8659f146e9c883e576803817f3ef3
donko <koiru3822fs@gmail.com>
2023-12-16 16:42:00 +0900

    依存パッケージのインストール方法を変更 (#8)

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,61 +1,48 @@
 macro(install_dependencies)
     # prerequisites
     include(FetchContent)
-    include(ExternalProject)
     find_program(MAKE_EXE NAMES gmake nmake make)
 
     if(CMAKE_BUILD_TYPE EQUAL "DEBUG")
         set(FETCHCONTENT_QUIET FALSE)
     endif()
 
+    set(grpc_VERSION v1.60.0)
+    message(STATUS "gRPC ${grpc_VERSION}")
     # install gRPC
     FetchContent_Declare(
         grpc
         GIT_REPOSITORY https://github.com/grpc/grpc.git
-        GIT_TAG v1.59.2
+        GIT_TAG 0ef13a7555dbaadd4633399242524129eef5e231 # v1.60.0
     )
     set(ABSL_PROPAGATE_CXX_STD ON)
     set(ABSL_ENABLE_INSTALL ON)
     FetchContent_MakeAvailable(grpc)
+    include_directories(${grpc_SOURCE_DIR} ${protobuf_SOURCE_DIR}/src ${absl_SOURCE_DIR})
 
+    set(cxxopts_VERSION v3.1.1)
+    message(STATUS "cxxopts ${cxxopts_VERSION}")
     # install cxxopts
     FetchContent_Declare(
         cxxopts
         GIT_REPOSITORY https://github.com/jarro2783/cxxopts.git
-        GIT_TAG v3.1.1
+        GIT_TAG eb787304d67ec22f7c3a184ee8b4c481d04357fd # v3.1.1
     )
     FetchContent_MakeAvailable(cxxopts)
+    include_directories(${cxxopts_SOURCE_DIR}/include)
 
+    set(csi-spec_VERSION v1.9.0+dev)
+    message(STATUS "csi-spec ${csi-spec_VERSION}")
     # install CSI spec
-    set(external_PREFIX "${CMAKE_CURRENT_BINARY_DIR}")
-    set(external_SOURCE_DIR "${external_PREFIX}/src")
-    set(external_INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/csi-spec")
-    ExternalProject_Add(
+    FetchContent_Declare(
         csi-spec
         GIT_REPOSITORY https://github.com/donkomura/csi-spec.git
-        GIT_TAG master
-        PREFIX "${external_PREFIX}"
-        SOURCE_DIR "${external_SOURCE_DIR}/csi-spec"
-        INSTALL_DIR "${external_INSTALL_DIR}"
-        UPDATE_COMMAND ""
-        BUILD_IN_SOURCE 1
-        CONFIGURE_COMMAND ""
-        BUILD_COMMAND ${MAKE_EXE} build_cpp PROTOC_VER=24.4
-        INSTALL_COMMAND sh -c "\
-            cp ${external_SOURCE_DIR}/csi-spec/lib/cxx/csi/* ${external_INSTALL_DIR} && \
-            cp ${external_SOURCE_DIR}/csi-spec/csi.proto ${external_INSTALL_DIR}"
+        GIT_TAG 2fa7f14134776e4b4585c420e23e8728b95ca62f # v1.9.0+dev
     )
-
-    if(external_INSTALL_DIR)
-        set(csi-spec_SOURCE_DIR "${external_SOURCE_DIR}/csi-spec")
-        set(csi-spec_INSTALL_DIR "${external_INSTALL_DIR}")
-        set(csi-spec_INCLUDE_DIR "${csi-spec_INSTALL_DIR}")
-        set(csi-spec_PROTOC_INCLUDE_DIR "${csi-spec_SOURCE_DIR}/lib/cxx/.protoc/include")
-        set(csi-spec_PROTO_GEN_SRC "${csi-spec_INSTALL_DIR}/csi.pb.cc")
-        set(csi-spec_PROTO_GEN_HDR "${csi-spec_INSTALL_DIR}/csi.pb.h")
+    FetchContent_GetProperties(csi-spec)
+    if (NOT csi-spec_POPULATED)
+        FetchContent_Populate(csi-spec)
     endif()
-
-    add_dependencies(grpc csi-spec)
 
     include(cmake/GrpcGen.cmake)
 endmacro()
