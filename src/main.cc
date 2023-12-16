cff33329d3671162e4ae20a1f83006bcd7fd5ea3
donkomura <koiru3822fs@gmail.com>
2023-12-16 13:16:29 +0000

    logger, driver-name

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,8 @@
+#include <plog/Appenders/ConsoleAppender.h>
+#include <plog/Formatters/TxtFormatter.h>
+#include <plog/Init.h>
+#include <plog/Log.h>
+
 #include <cxxopts.hpp>
 #include <fstream>
 #include <iostream>
@@ -5,6 +10,26 @@
 #include "service.h"
 
 const std::string VERSION_FILE = "VERSION";
+
+/**
+ * @fn
+ * GetVersion returns the version string from the version file.
+ * @param version_file the path of the version file.
+ * @return the version string, or empty string if the version file does not
+ * found
+ */
+const std::string GetVersion(std::string version_file) {
+  std::ifstream ifs(version_file);
+  std::string version;
+  if (ifs.fail()) {
+    PLOG_ERROR << "version file \'" << version_file << "\' does not found";
+    return "";
+  }
+  while (getline(ifs, version)) {
+    return version;
+  }
+  return "";
+}
 
 int main(int argc, char **argv) {
   cxxopts::Options options("chfsplugin", "CSI driver for CHFS");
@@ -14,6 +39,8 @@ int main(int argc, char **argv) {
     ("v,version", "version")
     ("e,endpoint", "CSI endpoint",
       cxxopts::value<std::string>()->default_value("unix://tmp/csi.sock"))
+    ("d,driver-name", "name of this CSI driver",
+      cxxopts::value<std::string>()->default_value("chfsplugin"))
   ;
 
   auto parsed = options.parse(argc, argv);
@@ -22,20 +49,14 @@ int main(int argc, char **argv) {
     exit(0);
   }
   if (parsed.count("version")) {
-    std::ifstream ifs(VERSION_FILE);
-    std::string version;
-    if (ifs.fail()) {
-      std::cout << "version file \'" << VERSION_FILE << "\' does not found"
-                << std::endl;
-      exit(1);
-    }
-    while (getline(ifs, version)) {
-      std::cout << version << std::endl;
-      exit(0);
-    }
+    const std::string version = GetVersion(VERSION_FILE);
   }
 
+  static plog::ConsoleAppender<plog::TxtFormatter> consoleAppender;
+  plog::init(plog::info, &consoleAppender);
+
   const std::string endpoint = parsed["endpoint"].as<std::string>();
+  const std::string driver_name = parsed["driver-name"].as<std::string>();
   csi::service::Config config(endpoint);
 
   csi::service::Server server(config);
