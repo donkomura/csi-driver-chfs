4eafddb69bce187e14b0ee94f160e686857f072e
donko <koiru3822fs@gmail.com>
2024-03-11 21:21:57 +0900

    fix typo and default driver name (#24)
    * fix target name
    
    * dockerfile for build chfscsiplugin
    
    * fix node service to handle chfs server
    
    * example yamls to deploy chfscsiplugin
    
    * fix README(draft)
    
    * remove chfs server input

--- src/config.cc
+++ src/config.cc
@@ -3,13 +3,11 @@
 namespace csi {
 namespace service {
 Config::Config(const std::string endpoint, const std::string driver_name,
-               const std::string node_id, const std::string version,
-               const std::string server_address)
+               const std::string node_id, const std::string version)
     : endpoint_(endpoint),
       driver_name_(driver_name),
       node_id_(node_id),
-      version_(version),
-      server_address_(server_address) {
+      version_(version) {
   mounter_ = std::make_shared<FuseMounter>();
 }
 Config::~Config() {}
--- src/config.h
+++ src/config.h
@@ -11,15 +11,13 @@ namespace service {
 class Config {
  public:
   Config(const std::string endpoint, const std::string driver_name,
-         const std::string node_id, const std::string version,
-         const std::string server_address);
+         const std::string node_id, const std::string version);
   ~Config();
 
   const std::string &driver_name() const { return driver_name_; }
   const std::string &endpoint() const { return endpoint_; }
   const std::string &node_id() const { return node_id_; }
   const std::string &version() const { return version_; }
-  const std::string &server_address() const { return server_address_; }
   std::shared_ptr<FuseMounter> mounter() const { return mounter_; }
 
  private:
@@ -27,7 +25,6 @@ class Config {
   std::string endpoint_;
   std::string node_id_;
   std::string version_;
-  std::string server_address_;
   std::shared_ptr<FuseMounter> mounter_;
 };
 
